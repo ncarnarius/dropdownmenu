810d0338ea0a6cd9418cfcd60410cdd00d3778d7
ncarnarius <39972269+ncarnarius@users.noreply.github.com>
2018-06-05 17:19:14 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,3 +2,14 @@ $(".dropdown-content a").hover( function() { // Changes the .image-holder's img 
     var value=$(this).attr('data-src');
     $(".image-holder img").attr("src", value);
 });
+
+$(".dropdown-content1 a").hover( function() { // Changes the .image-holder's img src to the src defined in .list a's data attribute.
+    var value=$(this).attr('data-src');
+    $(".image-holder1 img").attr("src", value);
+});
+
+$(".dropdown-content2 a").hover( function() { // Changes the .image-holder's img src to the src defined in .list a's data attribute.
+    var value=$(this).attr('data-src');
+    $(".image-holder2 img").attr("src", value);
+});
+
